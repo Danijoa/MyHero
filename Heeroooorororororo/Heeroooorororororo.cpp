742c213874106f8df2c4d8e61bcb208a686a21bc
Danijoa <danijoa1020@gmail.com>
2021-03-09 03:18:55 +0900

    update2

--- Heeroooorororororo/Heeroooorororororo.cpp
+++ Heeroooorororororo/Heeroooorororororo.cpp
@@ -1,4 +1,4 @@
-﻿// Heeroooorororororo.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+// Heeroooorororororo.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 
 #include <iostream>
@@ -9,14 +9,15 @@
 #include <string>
 //using namespace std;
 
-struct HeroData
+struct MonsterData
 {
-	std::string heroName;
-	int hp;
-	int totalHP;
-	int money = 0;
-	int playerX = 1;
-	int playerY = 1;
+	int pos[3][2];	//최대3마리
+};
+
+struct MazeData
+{
+	int width;
+	int height;
 };
 
 struct Difficulty
@@ -24,6 +25,18 @@ struct Difficulty
 	std::string diff[3] = { "초급[1]", "중급[2]", "고급[3]" };
 	int startHP[3] = { 50, 70, 100 };
 	int monsterNum;	//1~3마리
+	int maze[3][2] = { {5,10}, {10,15},{15,20} };
+};
+
+struct HeroData
+{
+	std::string heroName;
+	int hp;
+	int totalHP; 
+	int money = 0;
+	int playerX = 1;
+	int playerY = 1;
+	int playerMove = 0;
 };
 
 int main()
@@ -36,17 +49,22 @@ int main()
 	std::cout << "영웅 이름을 입력하세요: ";
 	std::cin >> myHero.heroName;
 
-	//난이도 선택, 기타 시작 정보 설정
+	//난이도 선택
 	Difficulty myDiff;
 	int d;
 	std::cout << "\n난이도를 선택해주세요: ";
 	std::cout << "[ 초급(1) 중급(2) 고급(3) ]\n→ ";
 	d = _getch();
 	std::cout << myDiff.diff[((int)d - 48) - 1] << " 선택하였습니다\n\n";
+
+	//난이도에 따른 시작 정보 설정
 	myHero.hp = myDiff.startHP[((int)d - 48) - 1];
 	myHero.totalHP = myDiff.startHP[((int)d - 48) - 1];
 	myDiff.monsterNum = ((int)d - 48);	
-
+	MazeData myMaze;
+	myMaze.height = myDiff.maze[((int)d - 48) - 1][0];
+	myMaze.width = myDiff.maze[((int)d - 48) - 1][1];
+	
 	//다음 화면으로 넘어가기
 	std::cout << "<<     '" << myHero.heroName << "'님 게임을 시작합니다!     >>\n\n";
 	Sleep(1000);
@@ -56,30 +74,37 @@ int main()
 	srand(time(NULL));
 
 	//maze판 만들기
-	char maze[10][15];
-	for (int i = 0; i < 10; i++)
+	char** maze = new char* [myMaze.height];
+	for (int i = 0; i < myMaze.height; i++)
 	{
-		for (int j = 0; j < 15; j++)
+		maze[i] = new char[myMaze.width];
+	}
+	for (int i = 0; i < myMaze.height; i++)
+	{
+		for (int j = 0; j < myMaze.width; j++)
 		{
-			if (i == 0 || i == 9 || j == 0 || j == 14)
+			if (i == 0 || i == (myMaze.height - 1) || j == 0 || j == (myMaze.width - 1))
 				maze[i][j] = '#';
 			else
 				maze[i][j] = ' ';
 		}
 	}
-
+	
 	//플레이어 시작점 설정
 	maze[myHero.playerX][myHero.playerY] = 'P';
 
 	//몬스터 위치 설정
 	int monNumCnt = 0;
 	int monX, monY;
+	MonsterData myMonster;
 	while (monNumCnt < myDiff.monsterNum)	//maze[1][1]~maze[8][13] 실사용 범위
 	{
-		monX = (rand() % 8) + 1;
-		monY = (rand() % 13) + 1;
+		monX = (rand() % (myMaze.height - 2)) + 1;
+		monY = (rand() % (myMaze.width - 2)) + 1;
 		if (maze[monX][monY] != 'P' && maze[monX][monY] != 'M')
 		{
+			myMonster.pos[monNumCnt][0] = monX;
+			myMonster.pos[monNumCnt][1] = monY;
 			monNumCnt++;
 			maze[monX][monY] = 'M';
 		}
@@ -89,10 +114,10 @@ int main()
 	{
 		//maze판 출력
 		std::cout << "\n";
-		for (int i = 0; i < 10; i++)
+		for (int i = 0; i < myMaze.height; i++)
 		{
 			std::cout << "    ";
-			for (int j = 0; j < 15; j++)
+			for (int j = 0; j < myMaze.width; j++)
 			{
 				std::cout << maze[i][j] << " ";
 			}
@@ -102,21 +127,17 @@ int main()
 
 		//플레이어 이동
 		int userInput = _getch();
-		//std::cout << "userInput: " << userInput << "\n";
-		//std::cout << "(char)userInput: " << (char)userInput << "\n";
+		myHero.playerMove++;
 		int nextX = myHero.playerX;
 		int nextY = myHero.playerY;
-		//std::cout << "(myHero.playerX, myHero.playerY): " << myHero.playerX << "," << myHero.playerY << "\n";
 		if ((char)userInput == 'w' && (myHero.playerX - 1) >= 1)  //키 조작
 			nextX = myHero.playerX - 1;
-		if ((char)userInput == 's' && (myHero.playerX + 1) <= 8)
+		if ((char)userInput == 's' && (myHero.playerX + 1) <= myMaze.height - 2)
 			nextX = myHero.playerX + 1;
 		if ((char)userInput == 'a' && (myHero.playerY - 1) >= 1)
 			nextY = myHero.playerY - 1;
-		if ((char)userInput == 'd' && (myHero.playerY + 1) <= 13)
+		if ((char)userInput == 'd' && (myHero.playerY + 1) <= myMaze.width - 2)
 			nextY = myHero.playerY + 1;
-		//std::cout << "(nextX, next): " << nextX << "," << nextY << "\n";
-		//Sleep(1000);
 
 		//몬스터 만난 경우
 		bool shopEnter = false;
@@ -258,10 +279,52 @@ int main()
 		//maze 정보 변경
 		maze[myHero.playerX][myHero.playerY] = ' ';	
 		maze[nextX][nextY] = 'P';
+		for (int i = 0; i < myDiff.monsterNum; i++)
+		{
+			if (nextX == myMonster.pos[i][0] && nextY == myMonster.pos[i][1])
+			{
+				myMonster.pos[i][0] = -1;
+				myMonster.pos[i][1] = -1;
+				break;
+			}
+		}
 		myHero.playerX = nextX, myHero.playerY = nextY;
 
 		//화면 지우기
 		std::system("cls");
+
+		//플레이어 두번 이동 시 몬스터 한칸 이동
+		int dir[4][2] = { {-1,0}, {1,0}, {0,-1}, {0,1} };	//상하좌우
+		if (myHero.playerMove % 2 == 0)
+		{
+			for (int i = 0; i < myDiff.monsterNum; i++)
+			{				
+				int curMonX = myMonster.pos[i][0];
+				int curMonY = myMonster.pos[i][1];
+				if (curMonX != -1 && curMonY != -1)
+				{
+					while (true)
+					{
+						int moveRand = rand() % 4;
+						int nextMonX = myMonster.pos[i][0] + dir[moveRand][0];
+						int nextMonY = myMonster.pos[i][1] + dir[moveRand][1];
+						if (nextMonX < 2 || nextMonX > myMaze.height - 2 || nextMonY < 2 || nextMonY > myMaze.width - 2 
+							|| maze[nextMonX][nextMonY] == 'M' || maze[nextMonX][nextMonY] == 'P')	//이동하지 못하는 경우
+						{
+							continue;
+						}
+						else
+						{
+							maze[curMonX][curMonY] = ' ';
+							maze[nextMonX][nextMonY] = 'M';
+							myMonster.pos[i][0] += dir[moveRand][0];
+							myMonster.pos[i][1] += dir[moveRand][1];
+						}
+						break;
+					}
+				}
+			}
+		}
 	}
 
 	return 0;
